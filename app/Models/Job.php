85e9adc5c00c3efc162ff33468d97338a3dfcb8a
unknown <341381@studet.mboutrecht.nl>
2025-10-31 14:00:35 +0100

    de gig factory werkend gemaakt

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -3,10 +3,14 @@
 namespace app\Models;
 
 use \Illuminate\Database\Eloquent\Model;
+use \Illuminate\Database\Eloquent\Factories\HasFactory;
 // use \Illuminate\Support\Arr;
 
 class Job extends Model
 {
+    use HasFactory;
+
     protected $table = '_job__listings_';
-    protected $fillabled = [];
+
+    protected $fillable = ['title', 'salary'];
 }
---
+++ database/factories/GigFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Gig>
+ */
+class GigFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->jobTitle() . ' cat',
+            'salary' => (string) fake()->numberBetween(30, 120) . ' cans of cat food'
+        ];
+    }
+}
